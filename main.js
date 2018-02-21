4503583cc897316627d6f320e76388f1c2b83217
Sherzod Salokhiddinov <sherzod.khu@gmail.com>
2018-02-21 16:24:51 +0900

    link to

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-/*var express = require("express");
+var express = require("express");
 var app = express();
 console.log("Server has started");
+
 app.get("/", function(req, res){
-  console.log("Someone made a request to /home");
-  res.send("Hi there!");
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 //comment
-app.get("/bye", function(req, res){
-  console.log("Someone made a request to /bye");
-  res.send("<b>Bye</b> bye!");
+app.get("/karaoke/beliver", function(req, res){
+  res.sendFile(path.join(__dirname + '/Karaoke/KARAOKE.html'));
 });
 //comment
 app.get("/dog", function(req, res){
@@ -18,18 +17,8 @@ app.get("/dog", function(req, res){
  
 
 app.listen(80);
-*/
+
 /*, '0.0.0.0', function(){
   console.log("server has started!!!");
 });*/
-
-var stuff = require('./musor1');
-
-console.log(stuff.counter(['hello', 'hi', 'hey']));
-console.log(stuff.adder(stuff.pi, 5));
-console.log(stuff.adder(5,4)); 
-
-
-<h1>
-  Sherzodbek
-</h1>
+ 
